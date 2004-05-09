83990f717109a66bba76ca26753e27c9c541b4b8
Jelmer Vernooij <jelmer@samba.org>
2004-05-09 12:13:48 +0100

    Reindent

--- ptb.c
+++ ptb.c
@@ -1,20 +1,20 @@
 /*
-	(c) 2004: Jelmer Vernooij <jelmer@samba.org>
+   (c) 2004: Jelmer Vernooij <jelmer@samba.org>
 
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #include <stdio.h>
 #include <unistd.h>
@@ -37,7 +37,7 @@ int ptb_read_font(int fd, struct ptb_font *dest) {
 	ret+=read(fd, unknown, 2);
 	ret+=read(fd, &dest->italic, 1);
 	ret+=read(fd, &dest->underlined, 1);
-	
+
 	return ret;
 }
 
@@ -46,14 +46,14 @@ int ptb_read_string(int fd, char **dest) {
 	guint16 length;
 	char *data;
 	read(fd, &shortlength, 1);
-	
+
 	/* If length is 0xff, this is followed by a guint16 length */
 	if(shortlength == 0xff) {
 		if(read(fd, &length, 2) < 2) return -1;
 	} else {
 		length = shortlength;
 	}
-	
+
 	if(length) {
 		data = malloc(length+1);
 		if(read(fd, data, length) < length) return -1;
@@ -83,61 +83,61 @@ static int ptb_read_header(int fd, struct ptb_hdr *hdr)
 
 	switch(hdr->classification) {
 	case CLASSIFICATION_SONG:
-	read(fd, unknown, 1); /* FIXME */
-	ptb_read_string(fd, &hdr->class_info.song.title);
-	ptb_read_string(fd, &hdr->class_info.song.artist);
+		read(fd, unknown, 1); /* FIXME */
+		ptb_read_string(fd, &hdr->class_info.song.title);
+		ptb_read_string(fd, &hdr->class_info.song.artist);
 
-	read(fd, &hdr->class_info.song.release_type, 1);
+		read(fd, &hdr->class_info.song.release_type, 1);
 
-	switch(hdr->class_info.song.release_type) {
-	case RELEASE_TYPE_PR_AUDIO:
-	read(fd, &hdr->class_info.song.release_info.pr_audio.type, 1);
-	ptb_read_string(fd, &hdr->class_info.song.release_info.pr_audio.album_title);
-	read(fd, &hdr->class_info.song.release_info.pr_audio.year, 2);
-	read(fd, &hdr->class_info.song.release_info.pr_audio.is_live_recording, 1);
-	break;
-	case RELEASE_TYPE_PR_VIDEO:
-	ptb_read_string(fd, &hdr->class_info.song.release_info.pr_video.video_title);
-	read(fd, &hdr->class_info.song.release_info.pr_video.is_live_recording, 1);
-	break;
-	case RELEASE_TYPE_BOOTLEG:
-		ptb_read_string(fd, &hdr->class_info.song.release_info.bootleg.title);
-		read(fd, &hdr->class_info.song.release_info.bootleg.day, 2);
-		read(fd, &hdr->class_info.song.release_info.bootleg.month, 2);
-		read(fd, &hdr->class_info.song.release_info.bootleg.year, 2);
+		switch(hdr->class_info.song.release_type) {
+		case RELEASE_TYPE_PR_AUDIO:
+			read(fd, &hdr->class_info.song.release_info.pr_audio.type, 1);
+			ptb_read_string(fd, &hdr->class_info.song.release_info.pr_audio.album_title);
+			read(fd, &hdr->class_info.song.release_info.pr_audio.year, 2);
+			read(fd, &hdr->class_info.song.release_info.pr_audio.is_live_recording, 1);
+			break;
+		case RELEASE_TYPE_PR_VIDEO:
+			ptb_read_string(fd, &hdr->class_info.song.release_info.pr_video.video_title);
+			read(fd, &hdr->class_info.song.release_info.pr_video.is_live_recording, 1);
+			break;
+		case RELEASE_TYPE_BOOTLEG:
+			ptb_read_string(fd, &hdr->class_info.song.release_info.bootleg.title);
+			read(fd, &hdr->class_info.song.release_info.bootleg.day, 2);
+			read(fd, &hdr->class_info.song.release_info.bootleg.month, 2);
+			read(fd, &hdr->class_info.song.release_info.bootleg.year, 2);
+			break;
+		case RELEASE_TYPE_UNRELEASED:
+			break;
+
+		default:
+			fprintf(stderr, "Unknown release type: %d\n", hdr->class_info.song.release_type);
+			break;
+		}
+
+		read(fd, &hdr->class_info.song.is_original_author_unknown, 1);
+		ptb_read_string(fd, &hdr->class_info.song.music_by);
+		ptb_read_string(fd, &hdr->class_info.song.words_by);
+		ptb_read_string(fd, &hdr->class_info.song.arranged_by);
+		ptb_read_string(fd, &hdr->class_info.song.guitar_transcribed_by);
+		ptb_read_string(fd, &hdr->class_info.song.bass_transcribed_by);
+		ptb_read_string(fd, &hdr->class_info.song.copyright);
+		ptb_read_string(fd, &hdr->class_info.song.lyrics);
+		ptb_read_string(fd, &hdr->guitar_notes);
+		ptb_read_string(fd, &hdr->bass_notes);
 		break;
-	case RELEASE_TYPE_UNRELEASED:
-	break;
-
-	default:
-		fprintf(stderr, "Unknown release type: %d\n", hdr->class_info.song.release_type);
-		break;
-	}
-
-	read(fd, &hdr->class_info.song.is_original_author_unknown, 1);
-	ptb_read_string(fd, &hdr->class_info.song.music_by);
-	ptb_read_string(fd, &hdr->class_info.song.words_by);
-	ptb_read_string(fd, &hdr->class_info.song.arranged_by);
-	ptb_read_string(fd, &hdr->class_info.song.guitar_transcribed_by);
-	ptb_read_string(fd, &hdr->class_info.song.bass_transcribed_by);
-	ptb_read_string(fd, &hdr->class_info.song.copyright);
-	ptb_read_string(fd, &hdr->class_info.song.lyrics);
-	ptb_read_string(fd, &hdr->guitar_notes);
-	ptb_read_string(fd, &hdr->bass_notes);
-	break;
 	case CLASSIFICATION_LESSON:
-	ptb_read_string(fd, &hdr->class_info.lesson.title);
-	ptb_read_string(fd, &hdr->class_info.lesson.artist);
-	read(fd, &hdr->class_info.lesson.style, 2);
-	read(fd, &hdr->class_info.lesson.level, 1);
-	ptb_read_string(fd, &hdr->class_info.lesson.author);
-	ptb_read_string(fd, &hdr->guitar_notes);
-	ptb_read_string(fd, &hdr->class_info.lesson.copyright);
-	break;
+		ptb_read_string(fd, &hdr->class_info.lesson.title);
+		ptb_read_string(fd, &hdr->class_info.lesson.artist);
+		read(fd, &hdr->class_info.lesson.style, 2);
+		read(fd, &hdr->class_info.lesson.level, 1);
+		ptb_read_string(fd, &hdr->class_info.lesson.author);
+		ptb_read_string(fd, &hdr->guitar_notes);
+		ptb_read_string(fd, &hdr->class_info.lesson.copyright);
+		break;
 
 	default:
-	fprintf(stderr, "Unknown classification: %d\n", hdr->classification);
-	break;
+		fprintf(stderr, "Unknown classification: %d\n", hdr->classification);
+		break;
 
 	}
 
