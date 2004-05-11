f1ac111a0ab25c4ea6f4fd9136f5d9ae710da20d
Jelmer Vernooij <jelmer@samba.org>
2004-05-12 00:38:05 +0100

    Valid lilypond files are generated now

--- ptb2ly.c
+++ ptb2ly.c
@@ -21,6 +21,10 @@
 #include <popt.h>
 #include "ptb.h"
 
+char *note_names[12] = {
+	"a", "ais", "b", "c", "cis", "d", "dis", "e", "f", "fis", "g", "gis"
+};
+
 void ly_write_header(FILE *out, struct ptbf *ret) 
 {
 	fprintf(out, "\\header {\n");
@@ -39,6 +43,100 @@ void ly_write_header(FILE *out, struct ptbf *ret)
 		if(ret->hdr.class_info.lesson.copyright) fprintf(out, "  copyright = \"%s\"\n", ret->hdr.class_info.lesson.copyright);
 	}
 	fprintf(out, "}\n");
+}
+
+void ly_write_position(FILE *out, struct ptb_position *pos)
+{
+	GList *gl = pos->linedatas;
+	int l = g_list_length(pos->linedatas);
+
+	if(l == 0) {/* Rest */
+		fprintf(out, " r ");
+		/* FIXME */
+	}
+	
+	/* Multiple notes */
+	if(l > 1) fprintf(out, " <");
+
+	while(gl) {
+		struct ptb_linedata *d = gl->data;
+		int string = d->tone / 0x20;
+		int note = d->tone % 0x20;
+		int i;
+		int j;
+		
+		switch(string) {
+		case 0: note+= 19; break;
+		case 1: note+= 14; break;
+		case 2: note+= 10; break;
+		case 3: note+= 5; break;
+		case 4: note+= 0; break;
+		case 5: note+= -7; break;
+		}
+		printf("Note: %d: %d\n",note, note / 12);
+		
+		fprintf(out, "%s", note_names[abs(note)%12]);
+
+		j = abs(note / 12);
+		if(note < 0) {
+			for(i = j; i < 1; i++) fprintf(out, ",");
+		} else if(note > 0) {
+			for(i = 1; i < j; i++) fprintf(out, "'");
+		}
+
+		/* String */
+		fprintf(out, "\\%d ", string+1);
+		gl = gl->next;
+	}
+
+	/* Multiple notes */
+	if(l > 1) fprintf(out, " >");
+}
+
+void ly_write_staff(FILE *out, struct ptb_staff *s) 
+{
+	GList *gl;
+
+	
+	fprintf(out, "\t\t\\context StaffGroup <<\n");
+	fprintf(out, "\t\t\t\\context Staff <<\n"
+			"\t\t\t\t\\clef \"G_8\"\n");
+	fprintf(out, "\t\t\\notes {\n");
+	fprintf(out, "\t\t\t");
+	gl = s->positions1;
+	while(gl) {
+		ly_write_position(out, (struct ptb_position *)gl->data);
+		gl = gl->next;
+	}
+	fprintf(out, "\n");
+	fprintf(out, "\t\t}\n");
+
+	fprintf(out, "\t\t\t>>\n");
+	fprintf(out, "\t\t\t\\context TabStaff <<\n");
+	fprintf(out, "\t\t\\notes {\n");
+	fprintf(out, "\t\t\t");
+	gl = s->positions1;
+	while(gl) {
+		ly_write_position(out, (struct ptb_position *)gl->data);
+		gl = gl->next;
+	}
+	fprintf(out, "\n");
+	fprintf(out, "\t\t}\n");
+
+
+	fprintf(out,"\t\t\t>>\n");
+	fprintf(out, "\t\t>>\n");
+}
+
+void ly_write_section(FILE *out, struct ptb_section *s) 
+{
+	GList *gl = s->staffs;
+	fprintf(out, "\t << \n");
+	while(gl) {
+		ly_write_staff(out, (struct ptb_staff *)gl->data);
+		gl = gl->next;
+	}
+	fprintf(out, "\t >> \n");
 }
 
 int ly_write_lyrics(FILE *out, struct ptbf *ret)
@@ -71,16 +169,20 @@ int ly_write_chords(FILE *out, struct ptbf *ret)
 int main(int argc, const char **argv) 
 {
 	FILE *out = stdout;
-	int have_lyrics, have_chords;
+	int have_lyrics;
 	struct ptbf *ret;
 	int debugging = 0;
+	GList *gl;
+	int instrument = 0;
 	int c;
 	char *output = NULL;
 	poptContext pc;
 	struct poptOption options[] = {
 		POPT_AUTOHELP
 		{"debug", 'd', POPT_ARG_NONE, &debugging, 0, "Turn on debugging output" },
-		{"outputfile", 'f', POPT_ARG_STRING, &output, 0, "Write to specified file", "FILE" },
+		{"outputfile", 'o', POPT_ARG_STRING, &output, 0, "Write to specified file", "FILE" },
+		{"regular", 'r', POPT_ARG_NONE, &instrument, 0, "Write tabs for regular guitar" },
+		{"bass", 'b', POPT_ARG_NONE, &instrument, 1, "Write tabs for bass guitar"},
 		POPT_TABLEEND
 	};
 
@@ -115,10 +217,9 @@ int main(int argc, const char **argv)
 	fprintf(out, "\\version \"1.9.8\"\n\n");
 
 	ly_write_header(out, ret);
-	have_chords = ly_write_chords(out, ret);
 	have_lyrics = ly_write_lyrics(out, ret);
 
-	fprintf(out, "\\score{\n");
+	fprintf(out, "\\score {\n");
 	fprintf(out, "       <<\n");
     if(have_lyrics) {
 		fprintf(out, "    \\addlyrics\n"
@@ -128,10 +229,11 @@ int main(int argc, const char **argv)
 		  " \\context Lyrics \\text\n");
 	}
 
-	if(have_chords) {
-		fprintf(out, " \\context ChordNames \\acc\n");
+	gl = ret->instrument[instrument].sections;
+	while(gl) {
+		ly_write_section(out, (struct ptb_section *)gl->data);
+		gl = gl->next;
 	}
-
 
 	fprintf(out, "   >>\n");
 
