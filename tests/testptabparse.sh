d45a3c50b357d1ca144e8adb64916979b77fb231
Jelmer Vernooij <jelmer@samba.org>
2005-03-05 15:03:57 +0100

    Improve testsuite a bit

--- tests/testptabparse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Test which files can't be parsed
-for I in $*
-do
-	echo "$I"
-	./ptbinfo "$I" > /dev/null 2>/dev/null
-	./ptb2xml -f -q "$I" || echo "xml: $I"
-	./ptb2ly -q "$I" || echo "ly: $I"
-done
